1effd63db050ac098d2751892dfa565e4b8b9a2b
wayne <world.wayno@gmail.com>
2014-02-19 00:44:17 -0500

    added form

--- AngularForm/Scripts/Angular/Controllers/FormBuilderController.js
+++ AngularForm/Scripts/Angular/Controllers/FormBuilderController.js
@@ -1,24 +1,41 @@
-﻿'use strict';
+﻿
 
-angular.module("app", ['builder', 'builder.components', 'validator.rules'])
+(function() {
+    'use strict';
+
+    angular.module("app", ['builder', 'builder.components', 'validator.rules'])
     .controller("formBuilderController", function ($scope,$builder, $validator) {
         var checkbox, textbox;
 
-        textbox = $builder.addFormObject('default', {
-            component: 'textInput',
-            label: 'Title',
-            description: 'Title',
-            placeholder: 'input section Title here',
-            required: true,
-            editable: false
-        });
+        //textbox = $builder.addFormObject('default', {
+        //    component: 'textInput',
+        //    label: 'Title',
+        //    description: 'Title',
+        //    placeholder: 'input section Title here',
+        //    required: true,
+        //    editable: false
+        //});
 
-        checkbox = $builder.addFormObject('default', {
-            component: 'checkbox',
-            label: 'Pets',
-            description: 'Do you have any pets?',
-            options: ['Dog', 'Cat']
-        });
+        //checkbox = $builder.addFormObject('default', {
+        //    component: 'checkbox',
+        //    label: 'Pets',
+        //    description: 'Do you have any pets?',
+        //    options: ['Dog', 'Cat']
+        //});
 
+        $scope.isShowScope = true;
         $scope.form = $builder.forms['default'];
-});
+        $scope.input = [];
+        $scope.defaultValue = {};
+       // $scope.defaultValue[textbox.id] = 'default value';
+        // $scope.defaultValue[checkbox.id] = [true, true];
+        return $scope.submit = function () {
+            return $validator.validate($scope, 'default').success(function () {
+                return console.log('success');
+            }).error(function () {
+                return console.log('error');
+            });
+        };
+    });
+
+}());
